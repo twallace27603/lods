6549055d0722255d944fc7d579578df0511e5bf0
twallace27603 <tswwci@hotmail.com>
2018-03-15 21:41:50 -0400

    Modified deployment to use A1 size VM and added a test web API endpoint.

---
+++ 001-Deploy/Task2-TestWebAPI.ps1
@@ -0,0 +1,12 @@
+﻿$username = "@lab.CloudPortalCredential(1).Username"
+$password = "@lab.CloudPortalCredential(1).Password"
+$secstr = New-Object -TypeName System.Security.SecureString
+$password.ToCharArray() | ForEach-Object {$secstr.AppendChar($_)}
+$cred = new-object -typename System.Management.Automation.PSCredential -argumentlist $username, $secstr
+
+Login-AzureRmAccount -Credential $cred -Debug -Force
+$vm = (Get-AzureRmVM -ResourceGroupName 001LODS -name serviceHost)[0]
+$nic = Get-AzureRmResource -ResourceId $vm.NetworkProfile.NetworkInterfaces[0].Id
+$pip = Get-AzureRmResource -ResourceId $nic.Properties.ipConfigurations[0].properties.publicIPAddress.id
+$ip = $pip.Properties.ipAddress
+Start-Process -FilePath  "http://$($ip):8080/users"
