dd06dc4b0ebc6933dff217babd8f1e14b6d24b42
twallace27603 <tswwci@hotmail.com>
2018-03-22 19:10:28 -0400

    Added DataServiceTestDeploy as a seperate repo

---
+++ 001-Deploy/ExamCode.ps1
@@ -0,0 +1,7 @@
+﻿$LabInstanceId = "6248046"
+$rg = Get-AzureRmResourceGroup | ? {($_.Tags -ne $null) -and ($_.Tags["LabInstance"] -eq $LabInstanceId)}
+$ns = Get-AzureRmRelayNamespace -ResourceGroupName $rg.ResourceGroupName
+$hybrid = Get-AzureRmRelayHybridConnection -ResourceGroupName $rg.ResourceGroupName -Namespace $ns.Name -ErrorAction SilentlyContinue -ErrorVariable $e
+$hybrid.ListenerCount -gt 0
+
+
