97cfbd2830be91f921ab92a69175fa7acd6ab4e7
twallace27603 <tswwci@hotmail.com>
2018-03-28 21:38:20 -0400

    Added exam questions, modified policy

---
+++ 001-Deploy/ExamQuestion-HybridConnection.ps1
@@ -0,0 +1,60 @@
+﻿param($LabInstanceId)
+
+#Variables
+$hcCreated = $false
+$hcNameCorrect = $false
+$hcTargetCorrect = $false
+$evidenceHeading = "Hybrid Connection"
+$evidence = ""
+
+#Get Resource Group
+$rg = Get-AzureRmResourceGroup | ? {$_.Tags.Count -gt 0 -and $_.Tags['LabInstance'] -eq $LabInstanceId} 
+#Retrieve hybrid connection
+$relay = Get-AzureRmRelayNamespace -ResourceGroupName $rg.ResourceGroupName
+
+#Handle the case where a student creates more than one connection
+$hybrids = Get-AzureRmRelayHybridConnection -ResourceGroupName $rg.ResourceGroupName -Namespace $relay.Name  -ErrorAction SilentlyContinue -ErrorVariable $e
+if ($hybrids -ne $null) {
+    $hcCreated = $true
+    $hybrid = $hybrids | ? {$_.Name -eq "serviceHost"} 
+    if ($hybrid -ne $null) {
+            $hcNameCorrect = $true
+            $hcTargetCorrect = $hybrid.UserMetadata.ToLower().Contains('"value":"servicehost:8080"')
+    }
+}
+
+#Set success criteria
+$success = $hcCreated -and $hcNameCorrect -and $hcTargetCorrect
+
+#Set output
+if($success) {
+    $evidence = @"
+You have correctly created a hybrid connection:
+    Name: serviceHost
+    EndPoint: serviceHost
+    Port: 8080
+"@
+} else {
+    if(-not $hcCreated) {
+        $evidence = @"
+You have not created a hybrid connection.
+"@
+    } else {
+        $evidence = @"
+You have created a hybrid connection, but it is not correct.
+    Hybrid connection name: $($hybrid.Name) - This should be serviceHost
+    Hybrid connection Host/Port: $($hybrid.UserMetadata) - This should be serviceHost:8080
+"@
+    }
+}
+
+#Return output
+@"
+${evidenceHeading}
+$('-' * $evidenceHeading.Length)
+
+${evidence}
+"@
+
+#Return result
+$success
---
+++ 001-Deploy/ExamQuestion-HybridConnectionManager.ps1
@@ -0,0 +1,58 @@
+﻿param($LabInstanceId)
+
+
+#Variables
+$hcCreated = $false
+$hcm = $false
+$evidenceHeading = "Hybrid Connection Manager"
+$evidence = ""
+
+#Get Resource Group
+$rg = Get-AzureRmResourceGroup | ? {$_.Tags.Count -gt 0 -and $_.Tags['LabInstance'] -eq $LabInstanceId} 
+#Retrieve hybrid connection
+$relay = (Get-AzureRmRelayNamespace -ResourceGroupName $rg.ResourceGroupName)[0]
+$hybrids = Get-AzureRmRelayHybridConnection -ResourceGroupName $rg.ResourceGroupName -Namespace $relay.Name  -ErrorAction SilentlyContinue -ErrorVariable $e
+if ($hybrids -ne $null) {
+    $hcCreated = $true
+    $hybrid = $hybrids | ? {$_.Name -eq "serviceHost"} 
+    if ($hybrid -ne $null) {
+        $hcm = $hybrid.ListenerCount -eq 1
+    }
+}
+
+#Set success criteria
+$success = $hcCreated -and $hcm
+
+#Set output
+if($success) {
+    $evidence = @"
+You have correctly created a hybrid connection manager:
+    Host: hcmHost
+    Name: serviceHost
+    EndPoint: serviceHost
+    Port: 8080
+"@
+} else {
+    if(-not $hcCreated) {
+        $evidence = @"
+You have not created a hybrid connection.
+"@
+    } else {
+        $evidence = @"
+You have created a hybrid connection, but have not configured a hybrid connection manager.
+
+The hybrid connection manager should be installed on the hcmHost virtual machine and manage the serviceHost hybrid connection.
+"@
+    }
+}
+
+#Return output
+@"
+${evidenceHeading}
+$('-' * $evidenceHeading.Length)
+
+${evidence}
+"@
+
+#Return result
+$success
